e6193288f5adb7c8faf9b9dbe1b04335cc800ac1
carlosab2021 <cabaez2011@gmail.com>
2023-09-22 00:41:41 -0300

    desa9

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from app.app_porcentaje import app_porcentaje
 from app.app_genero import app_genero
 from app.app_genero_horas import app_genero_horas
-from app.app_desarrollador import app_desarrollador
 from app.app_userdata import app_userdata
 
 app = FastAPI()
@@ -11,5 +10,4 @@ app = FastAPI()
 app.include_router(app_porcentaje, prefix="/porcentaje", tags=["porcentaje"])
 app.include_router(app_genero, prefix="/genero", tags=["genero"])
 app.include_router(app_genero_horas, prefix="/genero_horas", tags=["genero_horas"])
-app.include_router(app_desarrollador, prefix="/desarrollador", tags=["desarrollador"])
 app.include_router(app_userdata, prefix="/user_data", tags=["user_data"])
